f57ca1f91641e0d3d82ce62efa464962afc4a7cb
SALONEE MEWADA <144507585+salonee-2004@users.noreply.github.com>
2024-09-30 14:16:43 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,35 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
-};
+    const romanMap = {
+        'I' = 1,
+        'V' = 5,
+        'X' = 10,
+        'L' = 50,
+        'C' = 100,
+        'D' = 500,
+        'M' = 1000
+    };
 
+    let total = 0;
+    let prevValue = 0;
+
+    for(let i = s.length - 1; i>=0; i--) {
+        const currentValue = romanMap[s[i]];
+
+        if (currentValue < prevValue)
+        {
+            total -= currentValue;
+        }
+        else { 
+            total += currentValue
+        }
+
+        prevValue = currentValue;
+    }
+
+    return total;
+
+};
 
 module.exports={romanToInt}
